1b9a649eff140a2fe3c5d0e8297aab2fa998e846
JaviParMu <mrjavistinson@gmail.com>
2023-12-03 00:13:07 +0100

    fix: search bar loading

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -30,15 +30,16 @@ const SearchBar = () => {
   }
   
   return (
-    <div className='flex items-center justify-center gap-4 mt-10'>
-      <div className='w-8 h-8'>
-        { (isPending && steamId !== '') ? <Spinner /> : <div /> }
+    <div className='flex items-center justify-center mt-10'>
+      <div className='relative w-[200px] lg:w-[300px]'>
+        <div className='absolute top-1 -left-12 w-8 h-8'>
+          { (isPending && steamId !== '') ? <Spinner /> : <div /> }
+        </div>
+        <search className='flex flex-col md:flex-row items-center md:items-end justify-center gap-2 md:gap-0'>
+          <input value={steamId} onChange={(e) => setSteamId(e.target.value)} className='relative md:mt-15 text-black rounded-lg px-4 py-2' type="text" placeholder="Enter steam ID" name="search" />
+          <button onClick={() => handleSearch(steamId)} className='rounded-lg px-4 py-2 bg-lime-600 ml-0 md:ml-3 lg:ml-5 font-semibold'>Search</button>
+        </search>
       </div>
-      <search className='flex flex-col md:flex-row items-center md:items-end justify-center gap-2 md:gap-0'>
-        <input value={steamId} onChange={(e) => setSteamId(e.target.value)} className='md:mt-15 text-black rounded-lg px-4 py-2' type="text" placeholder="Enter steam ID" name="search" />
-        <button onClick={() => handleSearch(steamId)} className='rounded-lg px-4 py-2 bg-lime-600 ml-0 md:ml-3 lg:ml-5 font-semibold'>Search</button>
-      </search>
-      <div className='w-8 h-8'></div>
     </div>
   )
 }
