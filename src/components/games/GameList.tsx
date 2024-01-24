4c94101529189bef8d30494e467fc0dea7eeb7f1
javiparmu <mrjavistinson@gmail.com>
2024-01-24 22:17:19 +0100

    fix: example data message

--- src/components/games/GameList.tsx
+++ src/components/games/GameList.tsx
@@ -10,12 +10,14 @@ interface GameListProps {
     steamId: string
 }
 
+const exampleId = 'example-steam-id'
+
 const GameList = async ({ steamId, }: GameListProps) => {
   let gameList: Game[] = []
 
   if (isEmpty(steamId)) {
     gameList = gamesDataExample
-    steamId = 'example-steam-id'
+    steamId = exampleId
   } else { 
     gameList = await searchGames(steamId)
   }
@@ -26,7 +28,7 @@ const GameList = async ({ steamId, }: GameListProps) => {
 
   return (
     <section className='mt-10'>
-      {isEmpty(steamId) && <p className='text-lg text-gray-400 mb-4'>This is example data, use your Steam ID to see your runs.</p>}
+      {steamId === exampleId && <p className='text-lg text-gray-400 mb-4'>This is example data, use your Steam ID to see your runs.</p>}
       {gameList.length > 0
         ? <div className={style}>
           {gameList.map((game: Game) => {
